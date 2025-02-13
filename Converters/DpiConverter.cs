02f2078f8edbcb9a91a9570789c8be13030364e2
Bálint Juhász <juhaszbalintjozsef@gmail.com>
2025-02-14 00:29:07 +0100

    working on MainWindow DPI awareness

---
+++ Converters/DpiConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DriveSync.WPF.Converters
+{
+    public class DpiConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double originalValue)
+            {
+                var dpiScale = VisualTreeHelper.GetDpi(Application.Current.MainWindow).DpiScaleX;
+                return originalValue * (96.0 / (96.0 * dpiScale));
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Converters/DpiHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace DriveSync.WPF.Helpers
+{
+    public static class DpiHelper
+    {
+        private const double DefaultDpi = 96.0;
+
+        public static double GetScalingFactor(Visual visual)
+        {
+            var source = PresentationSource.FromVisual(visual);
+            if (source?.CompositionTarget != null)
+            {
+                return source.CompositionTarget.TransformToDevice.M11;
+            }
+            return 1.0;
+        }
+
+        public static double ScaleValue(double value, Visual visual)
+        {
+            var scalingFactor = GetScalingFactor(visual);
+            return value * (DefaultDpi / (DefaultDpi * scalingFactor));
+        }
+
+        public static Thickness ScaleThickness(Thickness thickness, Visual visual)
+        {
+            var scalingFactor = GetScalingFactor(visual);
+            var scale = DefaultDpi / (DefaultDpi * scalingFactor);
+            return new Thickness(
+                thickness.Left * scale,
+                thickness.Top * scale,
+                thickness.Right * scale,
+                thickness.Bottom * scale);
+        }
+
+        public static double GetDpiScale(Visual visual)
+        {
+            var source = PresentationSource.FromVisual(visual);
+            if (source?.CompositionTarget != null)
+            {
+                return DefaultDpi * source.CompositionTarget.TransformToDevice.M11 / DefaultDpi;
+            }
+            return 1.0;
+        }
+    }
+}
