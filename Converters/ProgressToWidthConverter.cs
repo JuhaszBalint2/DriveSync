e9b7044d378e6f85d4f2316434d7ff042831174c
Bálint Juhász <juhaszbalintjozsef@gmail.com>
2025-02-18 02:42:01 +0100

    Need to make Sync History Items clickable in new button/panel, then need to fill in the empty space on MainWindow with useful stats and info

---
+++ Converters/ProgressToWidthConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DriveSync.WPF.Converters
+{
+    public class ProgressToWidthConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 4 &&
+                values[0] is double value &&
+                values[1] is double minimum &&
+                values[2] is double maximum &&
+                values[3] is double actualWidth)
+            {
+                if (maximum - minimum == 0) return 0.0;
+                return actualWidth * (value - minimum) / (maximum - minimum);
+            }
+            return 0.0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
